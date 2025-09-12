a741a17f3daa86fdda9e5157573fa607697da205
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2025-09-11 20:08:10 -0400

    (feat) added VPC in network module w/ subnets

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,7 +1,3 @@
-/*resource "aws_s3_bucket" "test" {
-  bucket = "test-bucket-${random_id.suffix.hex}"
+module "network" {
+  source = "../../modules/network"
 }
-
-resource "random_id" "suffix" {
-  byte_length = 4
-}*/
